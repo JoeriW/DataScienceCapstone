3997b19ad38e325e1c0c6f7e9e4b9fdcb461c6c1
JoeriW <willems.joeri85@telenet.be>
2015-08-10 19:56:36 +0200

    progressed on app

--- application/functions.R
+++ application/functions.R
@@ -1,8 +1,10 @@
 library(stringi)
+library(tm)
 
 
 txt_cleaning <- function(text){
         txt <- text
+        txt <- removeNumbers(txt)
         txt <- gsub("[\u0030|\u0031|\u0032|\u0033|\u0034|\u0035|\u0036|\u0037|\u0038|\u0039]","", txt)
         txt <- gsub("[\u0023|\u002A|\u003B|\u003A|\u0028|\u0029|\u002C|\u002E|\u003F|\u0021|\u201D|\u201C|\u002F|\u00B8|\u0153]","", txt)
         txt <- gsub("[\u002D|\u2026|\u2013|\u2014|\u2018|\u2019|\u00AB|\u00BB|\u207B|\u208B|\uFE63|\uFF0D|\uE002D|\u00BD|\u005C|\u0022|\u0040|\u003C|\u003E|\u005F|\u00ED]", "", txt)
@@ -21,7 +23,13 @@ txt_sizing <- function(text,size){
         return(txt)      
 }
 
+txt_profanitycheck <- function(text){
+        regEx <- paste0(bannedWords$V1,collapse="|")
+        txt <- gsub(regEx,"",text)
+        return(txt)
+}
 
-
-
-
+txt_prediction >- function(text){
+        
+        
+}
--- application/ui.R
+++ application/ui.R
@@ -2,24 +2,29 @@ library(shiny)
 
 shinyUI(fluidPage(
         titlePanel("Swift Key:John Hopkins Data science capstone project"),
-
+        
         mainPanel(
                 tabsetPanel(type = "tabs",
                             tabPanel("Prediction",
                                      br(),
+                                     h4("Instruction:"),
+                                     p("Enter your text in the box below. Profane/insulting words, numbers, punctuations and special characters will be filtered out."),
+                                     tags$b("Note:"),
+                                     p("only the last 3 valid words (i.e. excluding profane/insulting terms) of the input are relevant for the prediction algorithm."),
+                                     br(),
                                      textInput('inputText',"Enter text here:",value=" "),
                                      submitButton("predict next word",icon=icon("magic",lib="font-awesome")),
                                      br(),
+                                     h4("Top prediction:"),
                                      textOutput('cleanText')
-                                     ),
+                            ),
                             
                             
                             tabPanel("Algorithm"),
                             tabPanel("About",
                                      br(),
                                      p(HTML('<p>Code is available on my github page: <a href="https://github.com/JoeriW/DataScienceCapstone">https://github.com/JoeriW/DataScienceCapstone.'))
-                                     )
                             )
                 )
-        ))
-
+        )
+))
